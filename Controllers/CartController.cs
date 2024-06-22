aadee2e6df31d9ab38bfd7d8d18a1a081a051904
ahm1999 <ahmedmedo1022@gmail.com>
2024-06-22 16:27:13 +0300

    cart functionlities

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -11,18 +11,40 @@ namespace E_Commerce_MVC.Controllers
     {
         private readonly AppDbContext _context;
         private readonly IUserManeger _userManeger;
-        public CartController(AppDbContext context,IUserManeger userManeger)
+        public CartController(AppDbContext context, IUserManeger userManeger)
         {
             _context = context;
             _userManeger = userManeger;
         }
+
         [Authorize]
-        public async Task<IActionResult> GetCart() {
+        [HttpPost]
+        [Route("[controller]/RemovefromCart/{cartItemId:guid}")]
+        public async Task<IActionResult> RemoveFromCart([FromRoute] Guid cartItemId)
+        {
+
+
+            Cart cart = await _context.Carts.Include(c => c.cartItems)
+                                             .FirstOrDefaultAsync(u => u.UserId == _userManeger.GetUserId());
+            _context.CartItems.Remove(cart.cartItems.FirstOrDefault(ci => ci.Id == cartItemId));
+
+            await _context.SaveChangesAsync();
+            return Redirect("/cart/GetCart");
+
+
+
+
+        }
+
+
+        [Authorize]
+        public async Task<IActionResult> GetCart()
+        {
             var cart = await _context.Carts.Include(c => c.cartItems)
-                                           .ThenInclude(c =>c.Product)
-                                           .FirstOrDefaultAsync(c =>c.UserId == _userManeger.GetUserId());    
-                                    
-                                
+                                           .ThenInclude(c => c.Product)
+                                           .FirstOrDefaultAsync(c => c.UserId == _userManeger.GetUserId());
+
+
 
             return View(cart);
         }
@@ -31,22 +53,24 @@ namespace E_Commerce_MVC.Controllers
         [HttpPost]
         [Route("[controller]/AddToCart/{ProductId:guid}")]
 
-        public async Task<IActionResult> AddToCart([FromRoute] Guid ProductId ) {
+        public async Task<IActionResult> AddToCart([FromRoute] Guid ProductId)
+        {
 
             if (!await _context.Products.AnyAsync(p => p.Id == ProductId))
             {
                 return BadRequest();
             }
-            
 
-             var cart = await _context.Carts.FirstOrDefaultAsync(u => u.UserId == _userManeger.GetUserId() );
-            
-            Guid cartId = cart.Id; 
 
-            CartItem cartItem = new CartItem() { 
-            Id = Guid.NewGuid(),
-            ProductId = ProductId,
-            CartId = cartId
+            var cart = await _context.Carts.FirstOrDefaultAsync(u => u.UserId == _userManeger.GetUserId());
+
+            Guid cartId = cart.Id;
+
+            CartItem cartItem = new CartItem()
+            {
+                Id = Guid.NewGuid(),
+                ProductId = ProductId,
+                CartId = cartId
 
             };
             await _context.CartItems.AddAsync(cartItem);
